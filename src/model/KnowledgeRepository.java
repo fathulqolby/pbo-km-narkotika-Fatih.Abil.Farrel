967d9bd44e289477204fefddf4d6d35695e5f308
Fathul <fahulqolby6@gmail.com>
2026-06-27 15:06:41 +0700

    feat: inisiasi struktur dasar KnowledgeRepository dan ArrayList

---
+++ src/model/KnowledgeRepository.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.ArrayList;
+
+public class KnowledgeRepository {
+    private ArrayList<Putusan> daftarPutusan;
+
+    public KnowledgeRepository() {
+        daftarPutusan = new ArrayList<>();
+    }
+}
