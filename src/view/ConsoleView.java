af4ac28521edcce47c20db5d1b3ecded55346b5e
Farrel <farrelhafizd8@gmail.com>
2026-06-27 18:00:31 +0700

    feat: mendesain antarmuka menu utama CLI dan switch-case logic

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -13,4 +13,27 @@ public class ConsoleView {
         this.controller = new KnowledgeController();
         this.input = new InputHandler();
     }
+
+    public void mulai() {
+        boolean jalan = true;
+        while (jalan) {
+            System.out.println("\n=== KNOWLEDGE MANAGEMENT SYSTEM PUTUSAN ===");
+            System.out.println("1. Tampilkan Semua Putusan");
+            System.out.println("2. Cari Putusan (Nomor Perkara)");
+            System.out.println("3. Filter Putusan (Jenis Narkotika)");
+            System.out.println("4. Tambah Putusan Manual");
+            System.out.println("5. Keluar");
+
+            int pilihan = input.getIntInput("Pilih menu (1-5): ");
+
+            switch (pilihan) {
+                case 1: tampilkanSemua(); break;
+                case 2: cariPutusan(); break;
+                case 3: filterPutusan(); break;
+                case 4: tambahPutusan(); break;
+                case 5: jalan = false; System.out.println("Program selesai. Terima kasih!"); break;
+                default: System.out.println("Pilihan tidak ada di menu!");
+            }
+        }
+    }
 }
