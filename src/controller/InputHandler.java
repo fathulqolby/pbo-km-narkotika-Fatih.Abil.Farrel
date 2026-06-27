662e9e9067fc389b496481b0e45e97984df70031
shabildwi <sablindwigmailcom@webmail.umm.ac.id>
2026-06-27 17:02:50 +0700

    feat: menambahkan exception handling untuk input integer

--- src/controller/InputHandler.java
+++ src/controller/InputHandler.java
@@ -9,4 +9,22 @@ public class InputHandler {
     public InputHandler() {
         this.scanner = new Scanner(System.in);
     }
+
+    // Fungsi untuk mengamankan input angka bulat (Integer)
+    public int getIntInput(String pesan) {
+        int angka = 0;
+        boolean valid = false;
+        while (!valid) {
+            try {
+                System.out.print(pesan);
+                angka = scanner.nextInt();
+                valid = true;
+            } catch (InputMismatchException e) {
+                System.out.println("[Error] Input tidak valid! Harap masukkan angka.");
+                scanner.nextLine(); // Membersihkan sisa input yang salah
+            }
+        }
+        scanner.nextLine(); // Mengkonsumsi karakter newline (Enter) agar tidak lompat
+        return angka;
+    }
 }
