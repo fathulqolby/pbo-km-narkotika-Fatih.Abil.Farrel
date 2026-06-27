b93f771e1dc6152702dd9b8368b34383ea1ea979
Fathul <fahulqolby6@gmail.com>
2026-06-27 15:08:45 +0700

    feat: menambahkan fungsi simpan dan getDaftarSemua pada repository

--- src/model/KnowledgeRepository.java
+++ src/model/KnowledgeRepository.java
@@ -8,4 +8,21 @@ public class KnowledgeRepository {
     public KnowledgeRepository() {
         daftarPutusan = new ArrayList<>();
     }
+
+    // 1. Menyimpan data baru
+    public void simpan(Putusan p) {
+        daftarPutusan.add(p);
+    }
+
+    // 2. Mendapatkan semua data
+    public ArrayList<Putusan> getDaftarSemua() {
+        return daftarPutusan;
+    }
+
+    // 3. Mendapatkan total data saat ini
+    public int getTotalData() {
+        return daftarPutusan.size();
+    }
 }
+
+
