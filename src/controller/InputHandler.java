54fcae81803cd66f3c45c7629c5b0458b45085c3
shabildwi <sablindwigmailcom@webmail.umm.ac.id>
2026-06-27 17:00:43 +0700

    feat: membuat class InputHandler dan inisialisasi Scanner

---
+++ src/controller/InputHandler.java
@@ -0,0 +1,12 @@
+package controller;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InputHandler {
+    private Scanner scanner;
+
+    public InputHandler() {
+        this.scanner = new Scanner(System.in);
+    }
+}
