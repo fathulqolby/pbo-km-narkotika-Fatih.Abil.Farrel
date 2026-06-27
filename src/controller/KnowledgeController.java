36bc442b069fded5238d6ad5d5abaa0ee6b0a74f
shabildwi <sablindwigmailcom@webmail.umm.ac.id>
2026-06-27 16:45:18 +0700

    feat: inisialisasi class KnowledgeController dan deklarasi repository

---
+++ src/controller/KnowledgeController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import model.KnowledgeRepository;
+import model.Putusan;
+import util.PdfParserUtil;
+import java.util.ArrayList;
+
+public class KnowledgeController {
+    // Controller memegang akses ke repository (database)
+    private KnowledgeRepository repository;
+}
